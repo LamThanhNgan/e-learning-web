ab0420e7d8ee9cc692132108fa146f35b1519ebb
LAMTHANHNGAN <lamthanhngan662607004@gmail.com>
2021-12-13 06:50:41 +0700

    ngan update

--- GTEAMS/settings.py
+++ GTEAMS/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 import GTEAMS_APP
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -218,3 +218,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,  "static"),
 ]
+
+django_heroku.settings(locals())
